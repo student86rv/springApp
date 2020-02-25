1febb1197cc02eaf691635f0467732e1f18e204a
Roman <student.rv86@gmail.com>
2020-02-25 16:16:20 +0200

    add Spring configuration

--- src/main/java/ua/epam/springApp/AppConfig.java
+++ src/main/java/ua/epam/springApp/AppConfig.java
@@ -2,8 +2,10 @@ package ua.epam.springApp;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
+@EnableWebMvc
 @ComponentScan("ua.epam.springApp")
 public class AppConfig {
 }
